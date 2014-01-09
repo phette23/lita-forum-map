990b13d43725becdc72b8f5c29061699f4be9ab6
phette23 <phette23@gmail.com>
2014-01-09 18:39:18 -0500

    merge changes from master

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-var forumData = {},
+// shorthand used in window resize code
+var w = window,
+    d = document,
+    b = d.getElementsByTagName('body')[0],
+    e = d.documentElement,
+    forumData = {},
     // load data
     handleJSON = function (err, json) {
         if (err) {
@@ -37,26 +42,43 @@ var forumData = {},
         // transparency scale from the data
         var alpha = d3.scale.linear().domain([0, max4obj(forumData)]).range([0.3, 1]);
 
-        // fill in US states
+        // fill in states & provinces
         // use data to determine alpha value of rgba
         d3.selectAll('#USA > [id], #Canada > [id]').attr('fill',
             function (datum, index){
+                var attendees = getStateDatum(this);
                 // this = current DOM element
-                if (getStateDatum(this) === 0) {
+                if (attendees === 0) {
                     return '#ddd';
                 } else {
                     return 'rgba(256,0,0,' +
-                        alpha(getStateDatum(this)) +
+                        alpha(attendees) +
                         ')';
                 }
-        // add tooltip courtesy of hint.css
+        // add hover info
         }).append('title').text(
             function (datum) {
                 return this.parentNode.id.substr(3, 4) +
                     ' - ' +
                     getStateDatum(this.parentNode);
         });
+    },
+    // set SVG canvas based upon whichever constraint is greatest
+    fitScreen = function () {
+        var width = d.innerWidth || e.clientWidth || b.clientWidth,
+            height = w.innerHeight|| e.clientHeight|| g.clientHeight;
+        if ( width < height ) {
+            d3.select('#map').attr('width', width).attr('height', width);
+        } else {
+            d3.select('#map').attr('width', height).attr('height', height);
+        }
+    },
+    init = function () {
+        d3.json('2013data.json', handleJSON);
+        fitScreen();
+        // if user adjusts screen, resize map
+        d3.select(window).on('resize', fitScreen);
     };
 
 // this kicks everything off
-d3.json('2013data.json', handleJSON);
+init();
