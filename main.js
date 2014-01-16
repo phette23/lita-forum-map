b0526555a6abb09e9c68761ed48f1dc1885e8116
phette23 <phette23@gmail.com>
2014-01-16 18:31:17 -0500

    merge changes from master

--- main.js
+++ main.js
@@ -4,6 +4,73 @@ var w = window,
     b = d.getElementsByTagName('body')[0],
     e = d.documentElement,
     forumData = {},
+    // map state/province abbreviations to full names
+    abbr = {
+        "CA-AB": "Alberta",
+        "CA-BC": "British Columbia",
+        "CA-MB": "Manitoba",
+        "CA-NB": "New Brunswick",
+        "CA-NL": "Newfoundland and Labrador",
+        "CA-NT": "Northwest Territories",
+        "CA-NS": "Nova Scotia",
+        "CA-NU": "Nunavut",
+        "CA-ON": "Ontario",
+        "CA-PE": "Prince Edward Island",
+        "CA-QC": "Quebec",
+        "CA-SK": "Saskatchewan",
+        "CA-YT": "Yukon",
+        "US-AL": "Alabama",
+        "US-AK": "Alaska",
+        "US-AZ": "Arizona",
+        "US-AR": "Arkansas",
+        "US-CA": "California",
+        "US-CO": "Colorado",
+        "US-CT": "Connecticut",
+        "US-DE": "Delaware",
+        "US-DC": "District of Columbia",
+        "US-FL": "Florida",
+        "US-GA": "Georgia",
+        "US-HI": "Hawaii",
+        "US-ID": "Idaho",
+        "US-IL": "Illinois",
+        "US-IN": "Indiana",
+        "US-IA": "Iowa",
+        "US-KS": "Kansas",
+        "US-KY": "Kentucky",
+        "US-LA": "Louisiana",
+        "US-ME": "Maine",
+        "US-MD": "Maryland",
+        "US-MA": "Massachusetts",
+        "US-MI": "Michigan",
+        "US-MN": "Minnesota",
+        "US-MS": "Mississippi",
+        "US-MO": "Missouri",
+        "US-MT": "Montana",
+        "US-NE": "Nebraska",
+        "US-NV": "Nevada",
+        "US-NH": "New Hampshire",
+        "US-NJ": "New Jersey",
+        "US-NM": "New Mexico",
+        "US-NY": "New York",
+        "US-NC": "North Carolina",
+        "US-ND": "North Dakota",
+        "US-OH": "Ohio",
+        "US-OK": "Oklahoma",
+        "US-OR": "Oregon",
+        "US-PA": "Pennsylvania",
+        "US-RI": "Rhode Island",
+        "US-SC": "South Carolina",
+        "US-SD": "South Dakota",
+        "US-TN": "Tennessee",
+        "US-TX": "Texas",
+        "US-UT": "Utah",
+        "US-VT": "Vermont",
+        "US-VA": "Virginia",
+        "US-WA": "Washington",
+        "US-WV": "West Virginia",
+        "US-WI": "Wisconsin",
+        "US-WY": "Wyoming"
+    },
     // load data
     handleJSON = function (err, json) {
         if (err) {
@@ -16,9 +83,7 @@ var w = window,
     },
     // given state DOM el, return associated datum
     getStateDatum = function (el) {
-        // IDs on map are of form 'US-XX' or 'CA-XX'
-        // so last 2 chars are state/province abbr
-        var state = el.id.substr(3, 4);
+        var state = el.id;
         if (forumData[state]) {
             return forumData[state];
         } else {
@@ -58,9 +123,9 @@ var w = window,
         // add hover info
         }).append('title').text(
             function (datum) {
-                return this.parentNode.id.substr(3, 4) +
-                    ' - ' +
-                    getStateDatum(this.parentNode);
+                // use abbreviation map to get full name
+                var parent = this.parentNode;
+                return abbr[parent.id] + ' - ' + getStateDatum(parent);
         });
     },
     // set SVG canvas based upon whichever constraint is greatest
